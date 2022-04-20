8a2d26aa4336fc263776739e20f64d1f0be9aeff
Masayuki1009 <smasayukii1009@gmail.com>
2022-04-21 00:22:22 +0900

    start to make dev branch

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -18,7 +18,7 @@ export const Home = () => {
     e.preventDefault();
     try {
       if (todoInput === "") {
-        return;
+        return
       }
       const newTodo = { title: todoInput };
       clearInputs();
